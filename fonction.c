121e18c4f5147cb3b0a509835d5e9f02dacc1c10
AntoineV <antoine.science@gmail.com>
2012-07-18 19:32:04 +0200

    update

---
+++ fonction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonction.h"
+
+void afficher_status_joueur(int position, int de,int argent)
+{
+    printf("AVANT:Position:%d\tLancer:%d\t Argent:%d\n",position,de,argent);
+}
+void afficher_argent_joueur(int argent)
+{
+    printf("Vous avez %d$\n",argent);
+}
+void afficher_cout_case(int* status_case[][],int position)
+{
+    printf("Cout case:%d\n", status_case[position][]);
+}
---
+++ fonction.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void afficher_status_joueur(int position, int de,int argent);
+void afficher_cout_case(int* cout_case[], int position);
+void afficher_argent_joueur(int argent);
+
